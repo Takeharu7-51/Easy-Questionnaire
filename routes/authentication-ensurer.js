c716071c0edd1106033fecc32b91eba086e66f71
Takeharu7-51 <take7spring@gmail.com>
2021-01-27 14:51:00 +0900

    ログイン関係を変更

--- routes/authentication-ensurer.js
+++ routes/authentication-ensurer.js
@@ -4,7 +4,8 @@ function ensure(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect('/login?from=' + req.originalUrl);
+  // res.redirect('/login?from=' + req.originalUrl);
+  res.redirect('/');
 }
 
 module.exports = ensure;
